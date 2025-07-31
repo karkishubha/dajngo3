c5a684ba83e53b00f6fcb1f098f78c88837aa866
Shubha Raj Karki <karkishubha985@gmail.com>
2025-07-31 07:20:08 +0545

    used models

--- display_page/admin.py
+++ display_page/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from display_page.models import Person
+from display_page.models import musician
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(musician)
--- display_page/models.py
+++ display_page/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+def __str__(self):
+    return 'f{self.first_name} {self.last_name}'
+class musician (models.Model):
+    first_name = models.CharField(max_length=30, verbose_name="Full name")
+    age = models.PositiveBigIntegerField(help_text= 'Enter age')
+    instrument = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'musician'
