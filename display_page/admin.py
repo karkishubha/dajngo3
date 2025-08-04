b2aa9eb27677dcc9c56acf570f905b3e98b33d74
Shubha Raj Karki <karkishubha985@gmail.com>
2025-08-04 07:39:32 +0545

    relatioship migrations

--- display_page/admin.py
+++ display_page/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from display_page.models import Person
 from display_page.models import musician
+from display_page.models import citizenship
+from display_page.models import Car
+from display_page.models import Author
 
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(musician)
+admin.site.register(citizenship)
+admin.site.register(Car)
+admin.site.register(Author)
+
--- display_page/models.py
+++ display_page/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-def __str__(self):
-    return 'f{self.first_name} {self.last_name}'
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 class musician (models.Model):
     first_name = models.CharField(max_length=30, verbose_name="Full name")
     age = models.PositiveBigIntegerField(help_text= 'Enter age')
@@ -15,3 +15,37 @@ class musician (models.Model):
 
     class Meta:
         db_table = 'musician'
+Districts = [
+    ("ktm", "kathmandu"),
+    ("Bkt", "Bhaktapur"),
+    ("Llp", "Lalitpur"),
+    ("Phk", "Pokhara"),
+    
+]
+class citizenship (models.Model):
+    citizenship_number=models.PositiveIntegerField(max_length=30,primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    issued_date=models.DateField()
+    issued_date_my=models.DateField(auto_now_add=True)
+    district = models.CharField(max_length=30, choices=Districts)
+    person = models.OneToOneField(Person,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.citizenship_number}"
+class Car (models.Model):
+    
+    car_name = models.CharField(max_length=30)
+    
+    person = models.ForeignKey(Person,on_delete=models.CASCADE)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    
+
+    def __str__(self):
+        return f"{self.name}"
+class Articles(models.Model):
+    title=models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
